34f04ad20838108162c907a64c4f2ad089787579
caronteone <caronteone@yahoo.it>
2019-02-21 18:40:41 +0100

    grunt 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,9 @@
 module.exports = function(grunt) {
     
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     grunt.initConfig({
         sass: {
@@ -71,11 +73,92 @@ module.exports = function(grunt) {
                         dest: 'dist/'
                     }]
                 }
+            },
+            useminPrepare:{
+                foo: {
+                    dest: 'dist',
+                    src: ['contactus.html', 'aboutus.html','index.html']
+                },
+                options: {
+                    flow: {
+                        steps: {
+                            css: ['cssmin'],
+                            js: ['uglify']
+                        },
+                        post: {
+                            css: [{
+                                name: 'cssmin',
+                                createConfig : function(context, block){
+                                    var generated = context.options.generated;
+                                    generated.options = {
+                                        keepSpecialComments: 0, rebase: false
+                                    };
+                                }
+                            }]
+                        }
+                    }
+                }
+            },
+            concat:  {
+                options: {
+                    separator: ';'
+
+                },
+                uglify: {
+                    dist: {}
+                },
+                cssmin: {
+                    dist: {}
+                },
+                filerev: {
+                    options: {
+                        encoding: 'utf8',
+                        algorithm: 'md5',
+                        length: 20
+                    },
+                    release: {
+                        files: [{
+                            src: [
+                                'dist/js/*.js',
+                                'dist/css/*css'
+                            ]
+                        }]
+                    }
+                }
+            },
+            usemin: {
+                html: ['dist/contactus.html', 'dist/aboutus.html', 'index.html'],
+                options:{
+                    assetsDirs: ['dist', 'dist/css', 'dist/js']
+            },
+            htmlmin:{
+                dist:{
+                    options:{
+                        collapsedWhitespace: true
+                    },
+                    files: {
+                        'dist/index.html': 'dist/index.html',
+                        'dist/contactus.html': 'dist/contactus.html',
+                        'dist/aboutus.html': 'dist/aboutus.html'
+                    }
+                }
             }
+        }
         
 
     });
     grunt.registerTask('css',['sass']);
     grunt.registerTask('default',['browserSync','watch']);
-    grunt.registerTask('build',['clean', 'copy','imagemin'])
+    grunt.registerTask('build',[
+     'clean',
+     'copy',
+     'imagemin',
+     'useminPrepare',
+     'concat',
+     'cssmin',
+     'uglify',
+     'filerev',
+     'usemin',
+     'htmlmin'
+    ]);
 };
